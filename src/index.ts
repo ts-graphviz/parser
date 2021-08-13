4e3a9a4741652aaff80efc4c72585488943117e6
kamiazya <yuki@kamiazya.tech>
2021-08-14 00:37:11 +0900

    add tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { SyntaxError } from './dot.peggy';
 export { AST } from './ast';
 export { convert } from './convert';
-export { stringify } from './stringify';
 export { parse } from './parse';
 export { dot } from './dot';
